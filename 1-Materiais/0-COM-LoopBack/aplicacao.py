08a3634fb3ba432c7a4d2f09e4eefd3bf69a4c0e
corsi <corsiferrao@gmail.com>
2017-08-06 08:50:01 -0300

    removendo spacos

---
+++ 1-Materiais/0-COM-LoopBack/aplicacao.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+# Prof. Rafael Corsi
+#  Abril/2017
+#  Aplicação
+####################################################
+
+from enlace import *
+import time
+
+def main():
+    # Inicializa enlace
+    com = enlace("/dev/ttyACM0")           # Ubuntu
+    #com = enlace("/dev/tty.usbmodem1411")  # Mac
+    #com = enlace("COM3")                   # Windows
+    com.enable()
+
+    # Imagem a ser transmitida
+    imageR = "./imgs/imageC.png"
+
+    # Imagem a ser salva
+    imageW = "./imgs/recebida.png"
+
+    # Testa se porta foi aberta
+    if not com.fisica.port is None:
+
+        # Log
+        print("-------------------------")
+        print("Comunicação inicializada")
+        print("  porta : {}".format(com.fisica.name))
+        print("-------------------------")
+
+        # Carrega imagem
+        print ("Carregando imagem para transmissão :")
+        print (" - {}".format(imageR))
+        print("-------------------------")
+        txBuffer = open(imageR, 'rb').read()
+        txLen    = len(txBuffer)
+        print(txLen)
+
+        # Transmite imagem
+        print("Transmitindo .... {} bytes".format(txLen))
+        com.sendData(txBuffer)
+
+        # espera o fim da transmissão
+        while(com.tx.getIsBussy()):
+            pass
+
+        # Atualiza dados da transmissão
+        txSize = com.tx.getStatus()
+        print ("Transmitido       {} bytes ".format(txSize))
+
+        # Faz a recepção dos dados
+        print ("Recebendo dados .... ")
+        rxBuffer, nRx = com.getData(txLen)
+
+        # log
+        print ("Lido              {} bytes ".format(nRx))
+
+        # Salva imagem recebida em arquivo
+        print("-------------------------")
+        print ("Salvando dados no arquivo :")
+        print (" - {}".format(imageW))
+        f = open(imageW, 'wb')
+        f.write(rxBuffer)
+
+        # Fecha arquivo de imagem
+        f.close()
+
+        # Encerra comunicação
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com.disable()
+
+    else:
+        print("Falha ao abrir a porta")
+
+if __name__ == "__main__":
+    main()
---
+++ 1-Materiais/0-COM-LoopBack/enlaceRx.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#####################################################
+# Camada Física da Computação
+# Prof. Rafael Corsi
+#  Abril/2017
+#  Camada de Enlace
+####################################################
+
+# Importa pacote de tempo
+import time
+
+# Threads
+import threading
+
+# Class
+class RX(object):
+    """ This class implements methods to handle the reception
+        data over the p2p fox protocol
+    """
+    
+    def __init__(self, fisica):
+        """ Initializes the TX class
+        """
+        self.fisica      = fisica
+        self.buffer      = bytes(bytearray())
+        self.threadStop  = False
+        self.threadMutex = True
+        self.READLEN     = 1024
+
+    def thread(self):
+        """ RX thread, to send data in parallel with the code
+        """
+        while not self.threadStop:
+            if(self.threadMutex == True):
+                rxTemp, nRx = self.fisica.read(self.READLEN)
+                if (nRx > 0):
+                    self.buffer += rxTemp
+                time.sleep(0.001)
+
+    def threadStart(self):
+        """ Starts RX thread (generate and run)
+        """
+        self.thread = threading.Thread(target=self.thread, args=())
+        self.thread.start()
+
+    def threadKill(self):
+        """ Kill RX thread
+        """
+        self.threadStop = True
+
+    def threadPause(self):
+        """ Stops the RX thread to run
+
+        This must be used when manipulating the Rx buffer
+        """
+        self.threadMutex = False
+
+    def threadResume(self):
+        """ Resume the RX thread (after suspended)
+        """
+        self.threadMutex = True
+
+    def getIsEmpty(self):
+        """ Return if the reception buffer is empty
+        """
+        if(self.getBufferLen() == 0):
+            return(True)
+        else:
+            return(False)
+
+    def getBufferLen(self):
+        """ Return the total number of bytes in the reception buffer
+        """
+        return(len(self.buffer))
+
+    def getAllBuffer(self, len):
+        """ Read ALL reception buffer and clears it
+        """
+        self.threadPause()
+        b = self.buffer[:]
+        self.clearBuffer()
+        self.threadResume()
+        return(b)
+
+    def getBuffer(self, nData):
+        """ Remove n data from buffer
+        """
+        self.threadPause()
+        b           = self.buffer[0:nData]
+        self.buffer = self.buffer[nData:]
+        self.threadResume()
+        return(b)
+
+    def getNData(self, size):
+        """ Read N bytes of data from the reception buffer
+
+        This function blocks until the number of bytes is received
+        """
+        while(self.getBufferLen() < size):
+            time.sleep(0.05)
+
+        return(self.getBuffer(size))
+
+
+    def clearBuffer(self):
+        """ Clear the reception buffer
+        """
+        self.buffer = b""
+
+
